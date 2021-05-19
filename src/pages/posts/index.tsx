7ba1797c99bcb99527fef54ac6ff1f2fe44b5c9d
Marcelo Bueno <marcelorezendebueno@gmail.com>
2021-05-19 18:57:24 -0300

    Componente: ActiveLink

--- src/pages/posts/index.tsx
+++ src/pages/posts/index.tsx
@@ -2,10 +2,22 @@ import { GetStaticProps } from 'next'
 import Head from 'next/head'
 import { getPrismicClient } from '../../services/prismic'
 import Prismic from '@prismicio/client'
+import { RichText } from 'prismic-dom'
 
 import styles from './styles.module.scss'
 
-export default function Posts() {
+type Post = {
+  slug: string;
+  title: string;
+  excerpt: string;
+  updatedAt: string;
+}
+
+interface PostsProps {
+  posts: Post[]
+}
+
+export default function Posts({ posts }: PostsProps) {
   return(
     <>
       <Head>
@@ -14,21 +26,13 @@ export default function Posts() {
 
       <main className={styles.container}>
         <div className={styles.posts}>
-          <a href="/">
-            <time>18 de maio de 2021</time>
-            <strong>Pela primeira vez em 30 anos, grandes do Rio não trocam de técnico no Estadual</strong>
-            <p>Demorou, mas chegou o dia em que o futebol carioca pode se vangloriar em relação ao paulista, em que seus dirigentes podem dizer — mesmo que não seja exatamente verdade — que foram mais profissionais do que os colegas do outro lado da ponte aérea.</p>
-          </a>
-          <a href="/">
-            <time>18 de maio de 2021</time>
-            <strong>Pela primeira vez em 30 anos, grandes do Rio não trocam de técnico no Estadual</strong>
-            <p>Demorou, mas chegou o dia em que o futebol carioca pode se vangloriar em relação ao paulista, em que seus dirigentes podem dizer — mesmo que não seja exatamente verdade — que foram mais profissionais do que os colegas do outro lado da ponte aérea.</p>
-          </a>
-          <a href="/">
-            <time>18 de maio de 2021</time>
-            <strong>Pela primeira vez em 30 anos, grandes do Rio não trocam de técnico no Estadual</strong>
-            <p>Demorou, mas chegou o dia em que o futebol carioca pode se vangloriar em relação ao paulista, em que seus dirigentes podem dizer — mesmo que não seja exatamente verdade — que foram mais profissionais do que os colegas do outro lado da ponte aérea.</p>
-          </a>
+          { posts.map(post => (
+            <a key={post.slug} href="#">
+              <time>{post.updatedAt}</time>
+              <strong>{post.title}</strong>
+              <p>{post.excerpt}</p>
+            </a>
+          ))}
         </div>
       </main>
     </>
@@ -45,9 +49,22 @@ export const getStaticProps: GetStaticProps = async () => {
     pageSize: 100,
   })
 
-  console.log(JSON.stringify(response, null, 2))
+  const posts = response.results.map(post => {
+    return {
+      slug: post.uid,
+      title: RichText.asText(post.data.title),
+      excerpt: post.data.content.find(content => content.type === 'paragraph')?.text ?? '',
+      updatedAt: new Date(post.last_publication_date).toLocaleDateString('pt-BR', {
+        day: '2-digit',
+        month: 'long',
+        year: 'numeric'
+      })
+    }
+  })
 
   return {
-    props: {}
+    props: {
+      posts
+    }
   }
 }
