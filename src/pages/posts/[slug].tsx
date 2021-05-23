856e6b2862846060c071ec00d573f7caa50665c8
Marcelo Bueno <marcelorezendebueno@gmail.com>
2021-05-23 19:16:32 -0300

    Finalização do módulo

--- src/pages/posts/[slug].tsx
+++ src/pages/posts/[slug].tsx
@@ -41,7 +41,7 @@ export const getServerSideProps: GetServerSideProps = async ({ req, params }) =>
   const session = await getSession({ req })
   const { slug } = params
 
-  if (!session.activeSubsctiption) {
+  if (!session?.activeSubsctiption) {
     return {
       redirect: {
         destination: '/',
